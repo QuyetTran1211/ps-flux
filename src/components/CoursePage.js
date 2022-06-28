279c88586cf4155c6f281cdecdc6f3e74755881a
TranMinhQuyet <44421023+caigiha1@users.noreply.github.com>
2022-06-28 16:15:58 +0700

    how to use mock-api

---
+++ src/components/CoursePage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { getCourses } from '../api/courseApi'
+
+class CoursePage extends React.Component {
+  state = {
+      courses: [],
+  };
+
+  componentDidMount() {
+    getCourses().then(courses => this.setState({ courses }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Courses</h2>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author ID</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.state.courses.map( course => {
+              return <tr>
+                <td>{course.title}</td>
+                <td>{course.authorId}</td>
+                <td>{course.category}</td>
+              </tr>
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+export default CoursePage;
